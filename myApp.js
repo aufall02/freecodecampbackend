c9bc91c2daea72711c6981c54a67b0e3c328d980
aufall02 <aufaluzumaki@gmail.com>
2023-09-08 15:45:19 +0700

    add hello json

--- myApp.js
+++ myApp.js
@@ -2,17 +2,18 @@ let express = require('express');
 let app = express();
 // console.log("Hello World");
 app.use('/public', express.static(process.cwd() + '/public'));
-
-
 app.route('/')
     .get(function (req, res) {
         res.sendFile(process.cwd() + '/views/index.html');
     })
 
-app.route('/_api/hello-console')
-    .get(function (req, res, next) {
-        console.log('Hello Word');
-    });
+const data = {
+    "message" : "Hello json"
+}
+
+app.get('/json', (req, res) => {
+    res.json(data)
+})
 
 
 
