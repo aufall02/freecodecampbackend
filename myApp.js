226958184d7a9d421769ee0cddb29a5a8641c6f4
aufall02 <aufaluzumaki@gmail.com>
2023-09-08 17:30:13 +0700

    add env

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 // console.log("Hello World");
+// console.log(process.env.MESSAGE_STYLE);
 app.use('/public', express.static(process.cwd() + '/public'));
 app.route('/')
     .get(function (req, res) {
@@ -12,7 +13,16 @@ const data = {
 }
 
 app.get('/json', (req, res) => {
-    res.json(data)
+
+    const MESSAGE_STYLE = process.env.MESSAGE_STYLE
+    if (MESSAGE_STYLE === 'uppercase') {
+        res.json({
+            "message" : "HELLO JSON"
+        })
+    }else{
+        res.json(data)
+    }
+    
 })
 
 
