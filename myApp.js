43c2d15036ef046e91532caccf6dd7bfa57b963b
aufall02 <aufaluzumaki@gmail.com>
2023-09-08 15:29:44 +0700

    show console log

--- myApp.js
+++ myApp.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let app = express();
-
+console.log("Hello World");
 app.use('/public', express.static(process.cwd() + '/public'));
 
 
-app.get('/', (req, res) => {
-   
-    res.send(console.log("Hello Word"))
-})
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    })
 
 app.route('/_api/hello-console')
   .get(function(req, res, next) {
