321fb8637cf0e10cb579f730fc99789ceccd2818
aufall02 <aufaluzumaki@gmail.com>
2023-09-08 15:36:19 +0700

    show view html

--- myApp.js
+++ myApp.js
@@ -5,15 +5,15 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 
 app.route('/')
-    .get(function(req, res) {
-		//   res.sendFile(process.cwd() + '/views/index.html');
+    .get(function (req, res) {
+        res.sendFile(process.cwd() + '/views/index.html');
         res.send('Hello Express')
     })
 
 app.route('/_api/hello-console')
-  .get(function(req, res, next) {
-    console.log('Hello Word');
-  });
+    .get(function (req, res, next) {
+        console.log('Hello Word');
+    });
 
 
 
