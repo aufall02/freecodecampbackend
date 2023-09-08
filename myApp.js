be1c3005140365cc1f25d2a3d4c8cdc195748acb
aufall02 <aufaluzumaki@gmail.com>
2023-09-08 15:33:38 +0700

    add hello express

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+// console.log("Hello World");
 app.use('/public', express.static(process.cwd() + '/public'));
 
 
 app.route('/')
     .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
+		//   res.sendFile(process.cwd() + '/views/index.html');
+        res.send('Hello Express')
     })
 
 app.route('/_api/hello-console')
